4b973c93eb0065752a75758fc4357fae2db0fa32
Rahul Vuppala <55148380+Rahulvvsv@users.noreply.github.com>
2024-11-10 10:38:44 +0530

    feat(newdaysafety): admin panel UI done

--- app/testing_components/page.tsx
+++ app/testing_components/page.tsx
@@ -1,12 +1,14 @@
 import Navbar from "@/components/molecules/navbar";
 import Hero from "@/components/molecules/heroLanding";
 import { ProductForm } from "@/components/molecules/ProductForm/ProductForm";
+import { ProductList } from "@/components/molecules/listView";
 const First = () => {
   return (
     <>
       {/* <Navbar></Navbar> */}
       {/* <Hero></Hero> */}
-      <ProductForm></ProductForm>
+      {/* <ProductForm></ProductForm> */}
+      <ProductList></ProductList>
     </>
   );
 };
