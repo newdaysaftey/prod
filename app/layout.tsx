46882aa339a3eed47110eae8a25dbae551a8702e
rahul <newdaysafety@gmail.com>
2024-12-29 12:20:43 +0530

    fix(newdaysafety): title changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <title>NewdaySatey</title>
       <body className={inter.className}>
         <Providers>
           <Navbar></Navbar>
